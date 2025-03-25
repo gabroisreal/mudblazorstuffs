f269ebc7e7d95e61edb57213f9a16a4d48da8884
Gabriel Iuza Frost <109495931+gabroisreal@users.noreply.github.com>
2025-03-25 10:23:51 -0500

    doesn't work :(

--- MyApplication/obj/Debug/net8.0/MyApplication.AssemblyInfo.cs
+++ MyApplication/obj/Debug/net8.0/MyApplication.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyApplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2b09f094e6e13847897b49018c6186d0aa6e52b")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyApplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyApplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
